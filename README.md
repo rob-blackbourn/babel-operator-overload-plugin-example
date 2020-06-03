a756024f84403cc8892322ae891a9644a0a5ce2c
Rob Blackbourn <rob.blackbourn@googlemail.com>
2020-06-03 11:00:48 +0100

    Updated for 3.0.0

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # babel-operator-overload-plugin-example
 
 This project demonstrates how to use my [babel-operator-overload-plugin](https://github.com/rob-blackbourn/babel-operator-overload-plugin).
+
+## Usage
+
+```bash
+npm install
+npm start
+```
--- src/index.js
+++ src/index.js
@@ -5,14 +5,29 @@ class Point {
       this.y = y
   }
   
-  __add__(other) {
+  [Symbol.for('+')](other) {
       const x = this.x + other.x
       const y = this.y + other.y
       return new Point(x, y)
   }
 }
 
+// Check primitives still work
+const i1 = 3
+const i2 = 4
+const i3 = i1 + i2
+console.log(i3)
+
+// Check overloads work
 const p1 = new Point(5, 5)
 const p2 = new Point(2, 3)
 const p3 = p1 + p2
 console.log(p3)
+
+// What happens with nulls?
+const s1 = null
+const s2 = 'hello'
+const s3 = s1 + s2
+console.log(s3)
+
+console.log('Done')
